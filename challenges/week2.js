0b2c8d1fa68b15e914403e72b4209a4955394fb0
peaceoni <peaceoni49@yahoo.com>
2019-11-26 20:04:20 +0000

    week2

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,16 +1,25 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return (sandwich.fillings)
 }
-
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  
+   if (person.city == "Manchester")
+  {
+  return true
+  }
+  else {
+    return false
+  }
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  return Math.ceil(people/40);
 }
 
 function countSheep(arr) {
@@ -21,6 +30,10 @@ function countSheep(arr) {
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  const firstLetter = person.address.postCode[0];
+  const secondLetter = person.address.postCode[1];
+  const isAplhChar = isNaN(Number(secondLetter));
+  return firstLetter === "M" && !isAplhChar;
 }
 
 module.exports = {
